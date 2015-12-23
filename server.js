19c65637ba903e82a7aab53d0a1b0413eed98e00
Adam Levy <Alevy01@gmail.com>
2015-12-23 16:58:25 -0500

    Running into async issues when getting users register_date.

--- server.js
+++ server.js
@@ -6,12 +6,6 @@ var fs = require('fs')
 var bodyParser = require('body-parser')
 var hash = require('./pass').hash
 
-//Models
-require('./models/users.js');
-require('./models/organizations.js');
-var User = mongoose.model('users');
-var Org = mongoose.model('organizations');
-
 var app = express();
 
 app.use(bodyParser.json());
@@ -19,9 +13,40 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+require('./models/users.js');
+require('./models/organizations.js');
+var User = mongoose.model('users');
+var Org = mongoose.model('organizations');
+
+
+
+
 app.get('/', function(req, res){
 	res.send("hello, connected.");
 });
+
+function authenticate (name, pass, fn){
+	
+	User.find( {"username" : name}, function(err, user){
+		if(err){
+			console.log("Error, user does not exist. " + err);
+			return fn(new Error('Cannot find user.'));
+		}
+		//Uses the users registration date as their salt value
+		tempSalt = user.register_date.toString().replace(/\s+/g, '');	
+		
+		hash(pass, tempSalt, function(err, hash){
+			if(err) 
+				return fn(err);
+			if(hash.toString('hex') == user.hashcode)
+				return fn(null, user);
+		});
+	});
+	
+	
+
+	fn(new Error('User hashcodes do not match. Invalid Password'));
+} 
 
 app.post('/register', function(req, res){
 	var user = new User ({
@@ -34,6 +59,7 @@ app.post('/register', function(req, res){
 	tempSalt = user.register_date.toString().replace(/\s+/g, '');
 
 	hash(req.body.password, tempSalt, function(err, hash){
+		
 		if(err) return err;
 		user.hashcode = hash.toString('hex');
 
@@ -53,6 +79,17 @@ app.post('/register', function(req, res){
 	});
 
 });
+
+app.post('/login', function(req, res){
+	authenticate(req.body.username, req.body.password, function(err, user){
+		if(user){
+			console.log(user + " was successfully logged in!");
+			res.json({
+				success : true
+			});
+		}
+	});
+})
 
 
 app.post('/createOrg', function(req, res){
