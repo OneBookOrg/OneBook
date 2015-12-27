0039cbbe696f0643689ee5ce9bcc514166937c09
Adam Levy <Alevy01@gmail.com>
2015-12-26 22:51:31 -0500

    Sign in correctly obtains values from html

--- site/js/signin.js
+++ site/js/signin.js
@@ -1,18 +1,15 @@
 $(document).ready(function() {
-			
-            $("#signinSubmit").click(function(event){
-				
-               $.post( 
-                  "http://localhost:8000/login",
-                  {
-                   username: $('#username').value,
-                   password: $('#password').value
-              	  },
-                  function(data) {
-                     alert(data);
-                  }
-               );
-					
-            });
-				
-         });
+      
+    $("#signinSubmit").click(function(event){
+
+       $.post( 
+          'login',
+          {
+            username : $("#username").val(),
+            password : $("#password").val()
+          }
+       );
+  
+    });
+
+});
