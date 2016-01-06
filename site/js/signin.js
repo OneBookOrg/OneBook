b401f04308fa4affb1fb07d072b7b5a77ce0c6ae
Sagar <sagar.mistry@knights.ucf.edu>
2016-01-06 17:20:33 -0500

    Update sign-in Page
    Error message produces error alert on login

--- site/js/signin.js
+++ site/js/signin.js
@@ -1,19 +1,32 @@
-$(document).ready(function() {
-      
-    $("#signinSubmit").click(function(event){
+onReady: function(){
+  signin.authenticate();
+},
 
-       $.post( 
-          'login',
-          {
-            username : $("#username").val(),
-            password : $("#password").val()
-          },
-          function(){
-          	window.location.replace("/blank.html");
-          }
-       );
+function authenticate(){
+  var usernameInput = $("#username").val();
+  var passwordInput = $("#password").val();
 
-  
-    });
+  $.ajax({
+      type: "POST",
+      cache: "false",
+      url: "http://localhost:8000/login",
+      data: {username:usernameInput, password:passwordInput},
+      success:function(){
+        window.location.replace("/blank.html");
+      }
+      error:function(){
+        errorMessage();
+      }
+  });
+}
 
-});
+
+function errorMessage(){
+  var appendString =   "<div class='alert alert-danger alert-dismissable'>"+
+                                   "   <a class='close' data-dismiss='alert' href='#'>&times;"+
+                          +"</a>"+
+                        +"<i class='icon-remove-sign'></i>"+
+                              +"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mi nunc, rutrum quis tincidunt ac"+
+                        "</div>";
+ $.("#error").append(appendString);
+}
