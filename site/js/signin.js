eed9bb4c6fbde8986331c564024e9d058ad6f827
Sagar <sagar.mistry@knights.ucf.edu>
2016-01-06 17:46:23 -0500

    Update sign-in Page
    Redo: Added onClick Function for button

--- site/js/signin.js
+++ site/js/signin.js
@@ -1,32 +1,38 @@
-onReady: function(){
-  signin.authenticate();
-},
+var signin = {
+    onReady: function(){
+      signin.authenticate();
+   },
 
-function authenticate(){
-  var usernameInput = $("#username").val();
-  var passwordInput = $("#password").val();
+ function authenticate(){
+   $("#signinSubmit").on('click',function(){
+   
+      var usernameInput = $("#username").val();
+      var passwordInput = $("#password").val();
 
-  $.ajax({
-      type: "POST",
-      cache: "false",
-      url: "http://localhost:8000/login",
-      data: {username:usernameInput, password:passwordInput},
-      success:function(){
-        window.location.replace("/blank.html");
-      }
-      error:function(){
-        errorMessage();
-      }
-  });
-}
+          $.ajax({
+              type: "POST",
+              cache: "false",
+              url: "http://localhost:8000/login",
+              data: {username:usernameInput, password:passwordInput},
+              success:function(){
+                window.location.replace("/blank.html");
+              }
+              error:function(){
+                errorMessage();
+              }
+          });
+    }
+  },
+
+  function errorMessage(){
+    var appendString =   "<div class='alert alert-danger alert-dismissable'>"+
+                                     "   <a class='close' data-dismiss='alert' href='#'>&times;"+
+                            +"</a>"+
+                          +"<i class='icon-remove-sign'></i>"+
+                                +"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mi nunc, rutrum quis tincidunt ac"+
+                          "</div>";
+   $.("#error").append(appendString);
+  }
 
 
-function errorMessage(){
-  var appendString =   "<div class='alert alert-danger alert-dismissable'>"+
-                                   "   <a class='close' data-dismiss='alert' href='#'>&times;"+
-                          +"</a>"+
-                        +"<i class='icon-remove-sign'></i>"+
-                              +"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mi nunc, rutrum quis tincidunt ac"+
-                        "</div>";
- $.("#error").append(appendString);
-}
+};
