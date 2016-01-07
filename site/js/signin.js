b615d9f12df69b8437f081fc0c7ca4e9aadf2455
Adam Levy <Alevy01@gmail.com>
2016-01-06 20:42:24 -0500

    Sign-in will alert the user if they enter the wrong username/password combo.

--- site/js/signin.js
+++ site/js/signin.js
@@ -1,38 +1,21 @@
-var signin = {
-    onReady: function(){
-      signin.authenticate();
-   },
 
- authenticate: function(){
-   $("#signinSubmit").on('click',function(){
-   
-      var usernameInput = $("#username").val();
-      var passwordInput = $("#password").val();
+$(document).ready(function() {
+      
+    $("#signinSubmit").click(function(event){
 
-          $.ajax({
-              type: "POST",
-              cache: "false",
-              url: "http://localhost:8000/login",
-              data: {username:usernameInput, password:passwordInput},
-              success:function(){
-                window.location.replace("/blank.html");
-              },
-              error:function(){
-                signin.errorMessage();
-              }
-          });
-    }
-  },
+       $.post( "login", {username : $("#username").val(), password : $("#password").val()}, function(data){
+          if(data.success){
+            window.location.replace("/blank.html");
+          }
+          else{
+            alert("Login Failed. Make sure your username and password are correct.");
+          }
+       })
+        
 
-  errorMessage: function(){
-    var appendString =   "<div class='alert alert-danger alert-dismissable'>"+
-                                     "   <a class='close' data-dismiss='alert' href='#'>&times;"+
-                            +"</a>"+
-                          +"<i class='icon-remove-sign'></i>"+
-                                +"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mi nunc, rutrum quis tincidunt ac"+
-                          "</div>";
-   $.("#error").append(appendString);
-  }
+        
+    });
+
+});
 
 
-};
