59f012cd7fca6c31fc23ec7f3e4f07f4ad2f3d8b
Adam Levy <Alevy01@gmail.com>
2016-02-19 22:17:58 -0500

    Only one error message at a time

--- site/js/signin.js
+++ site/js/signin.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 });
 
 function addError(){
-
+  $("#error").empty();
     var appendString =   '<div class="alert alert-danger alert-dismissible" role="alert">'+
                             '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
                                 "<strong>Error:</strong> Username or Password provided was invalid. Please try again."+
